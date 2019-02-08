312db1ef4529009f804242c34a2a287115b4403f
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-07 20:52:47 -0500

    #79 Create GameFile class *created a serializable class named GameFile with 6 public fields *added a public field named saveFileName of type string *added a public field named cameraLocation of type CameraLocation (to be created) *created a public field named totalPlayTime of type decimal *created a public field named saveDate of type string (string because we should not be doing any real operations with the value, we will just be storing and displaying it) *created a public field named player of type Player (to be created) *created a public field named gameProgression of type game progression

---
+++ MonsterIsland/Assets/Scripts/GameFile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class GameFile {
+    public string saveFileName;
+    public CameraLocation cameraLocation;
+    public decimal totalPlayTime;
+    public string saveDate;
+    public Player player;
+    public GameProgression gameProgression;
+}
