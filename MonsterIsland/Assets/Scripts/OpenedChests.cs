335b2f4c3e400d9a03fc6b715b12e45ff9d8452d
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-07 21:47:23 -0500

    #92 Create OpenedChests class *Created a serializable class named OpenedChests *inside OpenedChests, added 10 public bool fields, two fields to represent the two chests in each area

---
+++ MonsterIsland/Assets/Scripts/OpenedChests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class OpenedChests : MonoBehaviour {
+    public bool plainsChest1;
+    public bool plainsChest2;
+    public bool jungleChest1;
+    public bool jungleChest2;
+    public bool desertChest1;
+    public bool desertChest2;
+    public bool underwaterChest1;
+    public bool underwaterChest2;
+    public bool skylandChest1;
+    public bool skylandChest2;
+}
