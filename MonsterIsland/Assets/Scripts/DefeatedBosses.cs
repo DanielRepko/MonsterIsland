873b7c196638a5fb9bf2ba646e2c03b0096daac7
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-07 21:21:00 -0500

    #91 Create DefeatedBosses class *created a serializable class called DefeatedBosses with 6 public fields, all of type bool *added a public bool field named plansBossDefeated *added a public bool field named jungleBossDefeated *added a public bool field named underwaterBossDefeated *added a public bool field named desertBossDefeated *added a public bool field named skylandBossDefeated *added a public bool field named castleBossDefeated

---
+++ MonsterIsland/Assets/Scripts/DefeatedBosses.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DefeatedBosses {
+    public bool plainsBossDefeated;
+    public bool jungleBossDefeated;
+    public bool desertBossDefeated;
+    public bool underwaterBossDefeated;
+    public bool skylandBossDefeated;
+    public bool castleBossDefeated;
+}
