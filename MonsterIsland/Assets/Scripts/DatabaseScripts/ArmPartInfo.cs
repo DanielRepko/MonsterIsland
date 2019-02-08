6256b1488b4172f3ba50b2ac2b06ff896746c73d
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-07 22:21:33 -0500

    #85 Create the ArmPartInfo class *created a serializable class called ArmPartInfo *added a public string field called monster *added a public string field called bicepSprite *added a public string field called foreArmSprite *added a public string field called handBackSprite *added a public string field called handFrontSprite *added a public string field called fingersOpenBackSprite *added a public string field called fingersOpenFrontSprite *added a public string field called fingersClosedBackSprite *added a public string field called fingersClosedFrontSprite

---
+++ MonsterIsland/Assets/Scripts/DatabaseScripts/ArmPartInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ArmPartInfo {
+    public string monster;
+    public string bicepSprite;
+    public string forearmSprite;
+    public string handBackSprite;
+    public string handFrontSprite;
+    public string fingersOpenBackSprite;
+    public string fingersOpenFrontSprite;
+    public string fingersClosedBackSprite;
+    public string fingersClosedFrontSprite;
+}
