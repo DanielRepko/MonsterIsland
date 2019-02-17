4eed2ddc4049201418cbe3a3e2dbeb52480877bb
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-17 13:22:22 -0500

    #112 Create showPartEditor method in MonsterMakerManager *inside of the MonsterMakerManager script, added a method called showPartEditor with a string parameter *set the method to the onClick of the HeadSlot button *this method iterates through all of the MonsterMaker canvas' children and deactivates all objects except the selected slot (for now the HeadSlot) and the PartEditor, the latter of which is Activated *set the Images inside of the HeadSlot to expand with their container so that I can easily make the slot bigger before centering it in the PartEditor ui *after some fiddling, I realized that I can only set this stretching succefully on the head and torso images, because since the two head images are the same dimensions, they can be resized while still lining up, unlike the arm and leg images, which each have independent dimensions *this means that I will have to re-export the arm and leg images so that their transparent boxes make each part section the same size, however I will do this after I get the functionality working for the Monster Maker, as whatever I will be doing to one slot will be the same for every slot, regardless of the image dimensions

--- MonsterIsland/Assets/Scripts/MonsterMaker/MonsterMakerManager.cs
+++ MonsterIsland/Assets/Scripts/MonsterMaker/MonsterMakerManager.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class MonsterMakerManager : MonoBehaviour {
+
+    public Button headSlot;
 	
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 
 	// Update is called once per frame
 	void Update () { 
 		
 	}
+
+    public void showPartEditor(string partType)
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.gameObject.name == (partType + "Slot"))
+            {
+                
+
+            } else if(child.gameObject.name == "PartEditor")
+            {
+                child.gameObject.SetActive(true);
+
+            } else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
 }
