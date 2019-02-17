2b23d5148dfcfbbda012b6a45135365ddd4124ff
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-02-17 14:37:34 -0500

    112 Add more fields and methods to PartSlot *added a string field named partType, which will primarily be used by the ArmSlot class to determine which hands and fingers to display *added a method called EnterPartEditor that scales and moves the slot to the center of the PartEditor UI *added a method called ExitPartEditor that scales and move the slot to its original position in the MonsterMaker UI

--- MonsterIsland/Assets/Scripts/MonsterMaker/PartSlot.cs
+++ MonsterIsland/Assets/Scripts/MonsterMaker/PartSlot.cs
@@ -7,10 +7,19 @@ using System;
 public abstract class PartSlot : MonoBehaviour {
     public MonsterPartInfo partInfo;
     public Vector3 originalPosition;
+    public string partType;
 
     abstract public void ChangePart(MonsterPartInfo newPart);
 
     abstract public void ChangePrimaryColor(string newColor);
 
     abstract public void ChangeSecondaryColor(string newColor);
+
+    public void EnterPartEditor()
+    {
+        gameObject.transform.localPosition = new Vector3(0, 50f, 0);
+        gameObject.transform.localScale = new Vector3(1.4f, 1.4f, 0);
+    }
+
+    abstract public void ExitPartEditor();
 }
