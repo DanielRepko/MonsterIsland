10be7e9ea42a606c161ff12d5d04931126f295f4
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-02-19 17:10:24 -0500

    #120 Create layers 'Player', 'Terrain' - Assign 'Primary' tilemap in 'Hub' scene to 'Terrain' layer - Assign 'Player' object to 'Player' layer - Remove Box Collider 2D from 'PlaceholderPlayer' - Apply Edge Collider 2D to 'Player' object - Implement basic left/right movement in 'PlayerController' script

--- MonsterIsland/Assets/Scripts/Physics/PlayerController.cs
+++ MonsterIsland/Assets/Scripts/Physics/PlayerController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
+
+    private Rigidbody2D rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Use this for initialization
-	void Start () {
+    void Start () {
 		
 	}
+	
+	// Update is called once per frame
+	void Update () {
 
-    // Update is called once per frame
-    void Update () {
+    }
 
-	}
-
+    private void FixedUpdate() {
+        if (Input.GetAxis("Horizontal") > 0) {
+            rb.velocity = new Vector2(20f, rb.velocity.y);
+        } else if (Input.GetAxis("Horizontal") < 0) {
+            rb.velocity = new Vector2(-20f, rb.velocity.y);
+        } else if (Input.GetAxis("Horizontal") == 0) {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+    }
 }
