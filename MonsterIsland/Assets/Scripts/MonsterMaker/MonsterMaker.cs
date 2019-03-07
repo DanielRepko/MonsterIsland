941c43a5a96746ea9dab66585c51f7156f8e657e
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-03-07 10:50:46 -0500

    84 Import Vulture monster head images

--- MonsterIsland/Assets/Scripts/MonsterMaker/MonsterMaker.cs
+++ MonsterIsland/Assets/Scripts/MonsterMaker/MonsterMaker.cs
@@ -26,7 +26,7 @@ public class MonsterMaker : MonoBehaviour {
         //this code is for testing purposes only
         collectedParts = new CollectedPartsInfo()
         {
-            collectedHeads = new string[] { "Mitch", "Robot" },
+            collectedHeads = new string[] { "Mitch", "Vulture" },
             collectedTorsos = new string[] { "Mitch", "Robot"},
             collectedRightArms = new string[] { "Mitch", "Robot" },
             collectedLeftArms = new string[] { "Mitch", "Robot" },
