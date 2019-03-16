cf6adcf4f372c95512e7fd98d86edbd3cd94bb13
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-16 02:02:02 -0400

    #252 Add rigidbody 2d to 'AirBubble' prefab - Set gravity of 'AirBubble' to -1 so it floats up - Modified Linear Drag of 'AirBubble' to reduce float speed - Update 'AirBubble' so if it touches a wall, it is deleted - Apply 'Ground' tag to 'Primary' tilemap

--- MonsterIsland/Assets/Scripts/AirBubble.cs
+++ MonsterIsland/Assets/Scripts/AirBubble.cs
@@ -15,7 +15,7 @@ public class AirBubble : MonoBehaviour {
 	}
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.tag == "Player") {
+        if(collision.tag == "Player" || collision.tag == "Ground") {
             Destroy(gameObject);
         }
     }
