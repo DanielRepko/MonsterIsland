945da12bd0334da1db42d5a09cdb649d6018faf9
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-16 00:03:32 -0400

    #252 Create list in 'BubblePlantManager' to store bubble plants - Create public variable 'BubblePrefab' to store an instance of the bubble to create - Create public variable 'TimeBetweenBubbles' to store requested amount of time between bubble spawns - Create private variable 'TimeSinceLastBubble' to store how long it's been since the last bubble spawned - Place all 'BubblePlant' objects into 'bubblePlant' variable in 'BubblePlantManager' - Create 'AirBubble' object in 'Underwater' scene for future editing

--- MonsterIsland/Assets/Scripts/BubblePlantManager.cs
+++ MonsterIsland/Assets/Scripts/BubblePlantManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BubblePlantManager : MonoBehaviour {
 
+    public List<GameObject> bubblePlants;
+    public float timeBetweenBubbles = 5f;
+    private float timeSinceLastBubble;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,6 @@ public class BubblePlantManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
