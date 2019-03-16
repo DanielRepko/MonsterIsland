a5872bff4869c8e55348aa7757a573ca21b7b32e
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-16 02:37:42 -0400

    #252 Remove placeholder bubble from 'Underwater' scene - Set bubble prefab for 'BubblePlant' manager - Update 'BubblePlantManager' to spawn bubbles at all bubble plants after specified time

--- MonsterIsland/Assets/Scripts/BubblePlantManager.cs
+++ MonsterIsland/Assets/Scripts/BubblePlantManager.cs
@@ -16,6 +16,12 @@ public class BubblePlantManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        timeSinceLastBubble += Time.deltaTime;
+        if(timeSinceLastBubble >= timeBetweenBubbles) {
+            foreach(GameObject bubblePlant in bubblePlants) {
+                GameObject bubble = (GameObject)Instantiate(bubblePrefab,new Vector3(bubblePlant.transform.position.x, bubblePlant.transform.position.y), Quaternion.identity);
+            }
+            timeSinceLastBubble -= timeBetweenBubbles;
+        }
 	}
 }
