63db2ed0b288316c81f7037b54d36919615f5a42
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-17 16:27:20 -0400

    #257 Update 'Nest' script to allow setting nest's level - Update 'Nest' script to allow setting nest's location - Update 'Nest' script to allow setting if nest is active - Create enum in 'Nest' for level names - Create enum in 'Nest' for level locations

--- MonsterIsland/Assets/Scripts/Nest.cs
+++ MonsterIsland/Assets/Scripts/Nest.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LevelName {
+    Hub, Plains, Desert, Underwater, Jungle, Skyland, Castle
+}
+
+public enum LevelPosition {
+    Start, Shop, Boss
+}
+
 public class Nest : MonoBehaviour {
+
+    public LevelName levelName;
+    public LevelPosition levelPosition;
+    public bool isActive;
 
 	// Use this for initialization
 	void Start () {
