6197ce602c98fb733ffe299fe026f1c954759f30
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-19 14:57:34 -0400

    #272 Create properties in 'SettingsManager' to reference all needed objects - Move 'SettingsManager' object into 'SettingsPanel' - Renamed many objects in 'SettingsMenu' for better identification

--- MonsterIsland/Assets/Scripts/Managers/SettingsManager.cs
+++ MonsterIsland/Assets/Scripts/Managers/SettingsManager.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class SettingsManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Header("Music Objects", order = 0)]
+    public GameObject musicActive10;
+    public GameObject musicActive20;
+    public GameObject musicActive30;
+    public GameObject musicActive40;
+    public GameObject musicActive50;
+    public GameObject musicActive60;
+    public GameObject musicActive70;
+    public GameObject musicActive80;
+    public GameObject musicActive90;
+    public GameObject musicActive100;
+
+    [Header("Sound Objects", order = 1)]
+    public GameObject soundActive10;
+    public GameObject soundActive20;
+    public GameObject soundActive30;
+    public GameObject soundActive40;
+    public GameObject soundActive50;
+    public GameObject soundActive60;
+    public GameObject soundActive70;
+    public GameObject soundActive80;
+    public GameObject soundActive90;
+    public GameObject soundActive100;
+
+    [Header("Control Objects", order = 2)]
+    public GameObject primaryButtonText;
+    public GameObject secondaryButtonText;
+    public GameObject leftButtonText;
+    public GameObject rightButtonText;
+    public GameObject jumpButtonText;
+    public GameObject interactButtonText;
+    public GameObject torsoButtonText;
+    public GameObject headButtonText;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
