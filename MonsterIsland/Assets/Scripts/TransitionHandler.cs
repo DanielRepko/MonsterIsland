e6d3d0b98642ba5beec251e388d733deff69006b
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-18 21:14:12 -0400

    #264 Create 'HubToPlainsTransition' object in 'Hub' scene - Apply temporary sprite to 'HubToPlainsTransition' object to make visible - Create 'TransitionHandler' script and add it to 'HubToPlainsTransition' - Set 'TransitionHandler' to have 2 values, a LevelName and a 'FromHub' boolean - Add 'OnTriggerEnter2D' to 'TransitionHandler' that transitions to a different level based on the set values

---
+++ MonsterIsland/Assets/Scripts/TransitionHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionHandler : MonoBehaviour {
+
+    public LevelName levelName;
+    public bool fromHub;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Player") {
+            switch(levelName) {
+                case LevelName.Plains:
+                    if(fromHub) {
+                        SceneManager.LoadScene("Plains");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                case LevelName.Desert:
+                    if (fromHub) {
+                        SceneManager.LoadScene("Desert");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                case LevelName.Underwater:
+                    if (fromHub) {
+                        SceneManager.LoadScene("Underwater");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                case LevelName.Jungle:
+                    if (fromHub) {
+                        SceneManager.LoadScene("Jungle");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                case LevelName.Skyland:
+                    if (fromHub) {
+                        SceneManager.LoadScene("Skyland");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                case LevelName.Castle:
+                    if (fromHub) {
+                        SceneManager.LoadScene("Castle");
+                    } else {
+                        SceneManager.LoadScene("Hub");
+                    }
+                    break;
+                default:
+                    SceneManager.LoadScene("Hub");
+                    break;
+            }
+        }
+    }
+}
