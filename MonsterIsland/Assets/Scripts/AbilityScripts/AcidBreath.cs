ee7ac52021b112d2db056799b666cb98e3de6c7a
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-03-24 17:58:15 -0400

    #226 Create AcidBreath script *created a new script called AcidBreath *added an int field called damage with a default value of 1 *added two float fields called lifeTime and lifeTimer, which are used to time when the attached gameObject should be destroyed *added two float fields called timerTillDamage and damageTimer, which are used to time when the acid cloud is ready to deal damage (the acid cloud will deal damage in increments of 0.3 deltaTime, this value will most likely change) *added a bool field called readyToDamage, which will be used to determine if the the cloud is ready to deal damage *added a string field called target, which will be used to check whether the cloud should be damaging enemies or the player

---
+++ MonsterIsland/Assets/Scripts/AbilityScripts/AcidBreath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidBreath : MonoBehaviour {
+
+    public int damage = 1;
+    public float lifeTime = 2;
+    private float lifeTimer = 0;
+    private bool readyToDamage = true;
+}
