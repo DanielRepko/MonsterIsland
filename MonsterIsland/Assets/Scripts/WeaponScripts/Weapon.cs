fa8b5af1d2273d188b2bcad571a46a49e4467c7d
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-03-25 18:14:52 -0400

    #296 Create Weapon script *created a new script called Weapon *added private string fields for the weapon name, desc, and type (melee or projectile) *added a public static int field called damage *added a public static float field called attackRange, which will be used to determine the range of melee weapons *added a public static float field called attackCooldown, which is essentially the attack speed *added a private Sprite field called weaponSprite *added a public static GameObject field called projectilePrefab, which will be used by projectile weapons to immediately instantiate the correct projectile *added a public static AbilityFactory.Ability field called attackDelegate, which will hold the attack method to use, depending on whether the weapon is of type melee or projectile *added public properties for all listed fields, each with a getter and setter *added two (for now) empty methods called MeleeAttack and ProjectileAttack, these will hold the methods for the melee and projectile attacks, which while use the populated fields to make sure the weapon has the appropriate damage, range, cooldown, etc. *this structure makes it possible to reuse this script for every weapon in the game, by creating instances of the class inside the to be created WeaponFactory

---
+++ MonsterIsland/Assets/Scripts/WeaponScripts/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    //these fields are mainly used to display information about the weapon
+    private string _weaponName;
+    public string WeaponName { get { return _weaponName; } set { _weaponName = value; } }
+
+    private string _weaponDesc;
+    public string WeaponDesc { get { return _weaponDesc; } set { _weaponDesc = value; } }
+
+    private string _weaponType;
+    public string WeaponType { get { return _weaponType; } set { _weaponType = value; } }
+
+    //holds the damage dealt by the weapon
+    public static int _damage;
+    public int Damage { get { return _damage; } set { _damage = value; } }
+
+    //holds the range of the attack, used for melee weapons
+    public static float _attackRange;
+    public float AttackRange { get { return _attackRange; } set { _attackRange = value; } }
+
+
+    //The cooldown on the weapon's attack (essentially attack speed)
+    public static float _attackCooldown;
+    public float AttackCooldown { get { return _attackCooldown; } set { _attackCooldown = value; } }
+
+    //the sprite to be used for the weapon
+    private Sprite _weaponSprite;
+    public Sprite WeaponSprite { get { return _weaponSprite; } set { _weaponSprite = value; } }
+
+    //the prefab to use for the projectile, if the weapon is a projectile type
+    public static GameObject _projectilePrefab;
+    public GameObject ProjectilePrefab { get { return _projectilePrefab; } set { _projectilePrefab = value; } }
+
+    //used to store the attack delegate for the weapon
+    //delegate is self-set based on the weapon type
+    //using the AbilityFactory delegate type just so that I don't have to parse between different delegates in PlayerController
+    public static AbilityFactory.Ability _attackDelegate;
+    public AbilityFactory.Ability AttackDelegate { get { return _attackDelegate; } }
+
+    public void MeleeAttack()
+    {
+
+    }
+
+    public void ProjectileAttack()
+    {
+
+    }
+
+}
