3e79430e9dba8c4035242b5b6ab991ca07011b2c
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-26 13:03:14 -0400

    #304 Create 'Inventory' script and attach to 'Player' object - Update 'Inventory' script to be a singleton - Add 'Money' field to 'Inventory' script to store how much money player has - Add 'Value' field to 'Coin' script to store how much a coin is worth - Update 'Coin' script so if the player touches it, it destroys itself and adds it's value to the inventory - Create 'Coin' layer for 'Coin' prefab - Update layer colissions so the coin only collides with 'Player', 'Water', and 'Terrain'

--- MonsterIsland/Assets/Scripts/Coin.cs
+++ MonsterIsland/Assets/Scripts/Coin.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
+    public int value = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,12 @@ public class Coin : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.transform.tag == "Player") {
+            Inventory.Instance.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+
 }
---
+++ MonsterIsland/Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public static Inventory Instance;
+
+    public int money;
+
+	// Use this for initialization
+	void Start () {
+		if(Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddMoney(int amount) {
+        money += amount;
+    }
+
+}
