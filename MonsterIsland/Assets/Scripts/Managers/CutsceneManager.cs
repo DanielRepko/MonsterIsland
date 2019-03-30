33e3281c91cb1a56259efa3824bbbd345be18493
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-30 18:04:43 -0400

    #327 Create duplicate of 'PlayerCamera' named 'CutsceneCamera' in 'Hub' scene - Move 'CutsceneCamera' out of 'Player' object - Create 'Timelines' folder in 'Assets' - Create 'ActivateDesertGem' timeline in 'Timelines' folder - Create 'CutsceneManager' object and script - Update 'CutsceneManager' to be part of 'Managers' prefab - Update 'CutsceneManager' to be a singleton that never unloads - Attach 'Director' component to 'CutsceneManager' - Attach 'ActivateDesertGem' timeline to director in 'CutsceneManager' - Create simple animation of 'DesertGem' color value

---
+++ MonsterIsland/Assets/Scripts/Managers/CutsceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour {
+
+    public static CutsceneManager Instance;
+
+    private void Awake() {
+        if(Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
