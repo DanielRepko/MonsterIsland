0e3d32c5b034d3cf618e25011764155a9a73b534
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-04-02 13:17:56 -0400

    #248 Create Drill Projectile prefab *imported a new image asset of the drill from the Robot's left hand *created prefab of the sprite consisting of a gameObject containing the sprite as a child *created an animation for the projectile that spins it on the x axis *created and applied a new script called DrillProjectile to the prefab *the drill script allows the projectile to go through enemies, and to travel through one wall before being destroyed

---
+++ MonsterIsland/Assets/Scripts/WeaponScripts/DrillProjectile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillProjectile : Projectile {
+
+    private bool hasGoneThroughWall;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            if (hasGoneThroughWall)
+            {
+                Destroy(gameObject);
+            }            
+        }
+        if (target == "Enemy")
+        {
+            if (collision.tag == "Enemy")
+            {
+                Enemy enemy = collision.GetComponent<Enemy>();
+                if (enemy != null && collision == enemy.hurtBox)
+                {
+                    enemy.TakeDamage(damage, Helper.GetKnockBackDirection(transform, collision.transform));
+                }
+            }
+        }
+        else if (target == "Player")
+        {
+            if (collision.tag == "Player")
+            {
+                if (collision == PlayerController.Instance.hurtBox)
+                {
+                    PlayerController.Instance.TakeDamage(damage, Helper.GetKnockBackDirection(transform, collision.transform));
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            hasGoneThroughWall = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+}
--- MonsterIsland/Assets/Scripts/WeaponScripts/Projectile.cs
+++ MonsterIsland/Assets/Scripts/WeaponScripts/Projectile.cs
@@ -12,37 +12,6 @@ public class Projectile : MonoBehaviour {
     private float unsetTriggerTime = 0.01f;
     private float unsetTriggerTimer = 0;
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.tag == "Ground")
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //    if(target == "Enemy")
-    //    {
-    //        if (collision.tag == "Enemy")
-    //        {
-    //            Enemy enemy = collision.GetComponent<Enemy>();
-    //            if (enemy != null && collision == enemy.hurtBox)
-    //            {
-    //                enemy.TakeDamage(damage, Helper.GetKnockBackDirection(transform, collision.transform));
-    //                Destroy(gameObject);
-    //            }
-    //        }
-    //    }
-    //    else if (target == "Player")
-    //    {
-    //        if (collision.tag == "Player")
-    //        {
-    //            if (collision == PlayerController.Instance.hurtBox)
-    //            {
-    //                PlayerController.Instance.TakeDamage(damage, Helper.GetKnockBackDirection(transform, collision.transform));
-    //                Destroy(gameObject);
-    //            }
-    //        }
-    //    }
-    //}
-
     private void FixedUpdate()
     {
         if(unsetTriggerTimer < unsetTriggerTime)
