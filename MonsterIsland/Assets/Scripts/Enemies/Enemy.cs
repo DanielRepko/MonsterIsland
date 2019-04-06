35f24f438702716979b5b5d940099b445ec9dad3
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-04-06 14:46:09 -0400

    #372 Add Attack method override to Dingus script *made the Dingus script a child of hte Enemy class *overrode the Attack method with the implementation for the BigBeak ability

--- MonsterIsland/Assets/Scripts/Enemies/Enemy.cs
+++ MonsterIsland/Assets/Scripts/Enemies/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : Actor {
     public bool alwaysDropPart = false;
     public string partToAlwaysDrop;
     public string equippedWeapon;
-    public AbilityFactory.ArmAbility armAttackDelegate;
+    public AbilityFactory.ArmAbility attackDelegate;
     public AbilityFactory.Ability abilityDelegate;
 
     public delegate void CheckDelegate();
@@ -68,7 +68,7 @@ public class Enemy : Actor {
         //attacking if aggro
         if (isAggro && PlayerIsInAttackRange())
         {
-            armAttackDelegate(Helper.PartType.RightArm);
+            attackDelegate(Helper.PartType.RightArm);
         }
 
         //running any necessary checks on the Enemy
@@ -101,7 +101,7 @@ public class Enemy : Actor {
         var legPartInfo = PartFactory.GetLegPartInfo(monsterName);
         rightArmInfo.equippedWeapon = equippedWeapon;
 
-        armAttackDelegate = Attack;
+        attackDelegate = Attack;
         abilityDelegate = Ability;
 
         //adding methods to be run in fixed update to the check delegate
@@ -365,14 +365,14 @@ public class Enemy : Actor {
                 if(equippedWeapon != "")
                 {
                     monster.rightArmPart.weapon.Damage = 0;
-                    armAttackDelegate(Helper.PartType.RightArm);
+                    attackDelegate(Helper.PartType.RightArm);
                     monster.rightArmPart.weapon.Damage = damage;
                 }
                 else
                 {
                     int rememberDamage = damage;
                     damage = 0;
-                    armAttackDelegate(Helper.PartType.RightArm);
+                    attackDelegate(Helper.PartType.RightArm);
                     damage = rememberDamage;
                 }                
                 return false;
