da03cc9bc541556befeb99578d0fe0ac17819451
Daniel Repko <Daniel.Repko83@stclairconnect.ca>
2019-04-07 17:00:54 -0400

    #421 Create WeaponPicker scripts and objects *created three scripts called WeaponPicker, WeaponPickerButton, and WeaponSlot *the WeaponPicker script will be used similarily to the PartEditor script, in that it will manage the general operations in the WeaponPicker *the WeaponPickerButton script will be used just like its monster part counterparts, in that it will be used to populate the WeaponScroll so that when clicked, they can equip their respective weapon onto the player *the WeaponSlot script will also be used like its monster part counterparts, in that it will be used to hold the weapon that the player has chosen to equip *created a WeaponPickerButton prefab to be used in the WeaponScroll

---
+++ MonsterIsland/Assets/Scripts/MonsterMaker/WeaponPicker/WeaponPicker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPicker : MonoBehaviour {
+
+    public WeaponSlot weaponSlot;
+    public Text weaponName;
+    public Text weaponType;
+    public Text weaponDesc;
+
+    public ScrollRect weaponScroll;
+    public string[] availableWeapons;
+
+    public void PopulateWeaponPicker()
+    {
+        var xOffset = 0f;
+        //iterating through all of the items in the availbleWeapons array
+        for (int i = 0; i < availableWeapons.Length; i++)
+        {
+
+            if (i == 0)
+            {
+                xOffset = 60;
+            }
+            else
+            {
+                xOffset += 120;
+            }
+            //loading the appropriate WeaponPicker prefab
+            var pickerButtonPrefab = new GameObject();
+            pickerButtonPrefab = Resources.Load<GameObject>("Prefabs/MonsterMaker/WeaponPickerButton");
+
+            //instantiating the picker button
+            var pickerButton = Instantiate(pickerButtonPrefab, Vector2.zero, Quaternion.identity);
+
+            //initializing the pickerButton and also saving the created Weapon
+            var weapon = pickerButton.GetComponent<WeaponPickerButton>().InitializePickerButton(availableWeapons[i]);
+            //setting the onClick listener to the pickerButton
+            pickerButton.GetComponent<Button>().onClick.AddListener(
+                delegate
+                {
+                    weaponSlot.ChangeWeapon(weapon);
+                    weaponName.text = weapon.WeaponName;
+                    weaponType.text = weapon.WeaponType;
+                    weaponDesc.text = weapon.WeaponDesc;
+                });
+
+            //getting the rect transform of the button
+            var pickerButtonTransform = pickerButton.GetComponent<RectTransform>();
+            pickerButtonTransform.SetParent(weaponScroll.content);
+            pickerButtonTransform.anchoredPosition = new Vector2(xOffset, 0);
+            weaponScroll.content.sizeDelta = new Vector2(weaponScroll.content.sizeDelta.x + 100.5f, weaponScroll.content.sizeDelta.y);
+        }
+    }
+}
---
+++ MonsterIsland/Assets/Scripts/MonsterMaker/WeaponPicker/WeaponSlot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour {
+
+	public void ChangeWeapon(Weapon weapon)
+    {
+
+    }
+}
