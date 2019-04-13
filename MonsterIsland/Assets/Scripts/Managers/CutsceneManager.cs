082785fbd1de515fe37c91ad07e005982a778c7f
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-04-12 21:02:13 -0400

    #511 Setup all hub cutscenes - Removed placeholder mitch parts from all instances of player prefab

--- MonsterIsland/Assets/Scripts/Managers/CutsceneManager.cs
+++ MonsterIsland/Assets/Scripts/Managers/CutsceneManager.cs
@@ -13,43 +13,61 @@ public class CutsceneManager : MonoBehaviour {
         director = GetComponent<PlayableDirector>();
         var bosses = GameManager.instance.gameFile.gameProgression.defeatedBosses;
         bool hasBeenDefeated = false;
+        bool desert = false;
+        bool water = false;
+        bool jungle = false;
+
         //Check if i've already been defeated. If I have, destroy myself.
-        switch (SceneManager.GetActiveScene().name)
-        {
+        switch (SceneManager.GetActiveScene().name) {
+            case "Hub":
+                if(GameManager.instance.gameFile.gameProgression.viewedCutscenes.desertGem) {
+                    desert = true;
+                    GameObject.Find("DesertGem").GetComponent<SpriteRenderer>().color = Color.yellow;
+                }
+                if (GameManager.instance.gameFile.gameProgression.viewedCutscenes.underwaterGem) {
+                    water = true;
+                    GameObject.Find("UnderwaterGem").GetComponent<SpriteRenderer>().color = Color.blue;
+                }
+                if (GameManager.instance.gameFile.gameProgression.viewedCutscenes.jungleGem) {
+                    jungle = true;
+                    GameObject.Find("JungleGem").GetComponent<SpriteRenderer>().color = Color.green;
+                }
+                if(desert && water && jungle) {
+                    GameObject.Find("HubToSkylandTransition").SetActive(true);
+                }
+                if(GameManager.instance.gameFile.gameProgression.defeatedBosses.skylandBossDefeated) {
+                    GameObject.Find("HubToCastleTransition").SetActive(true);
+                    GameObject.Find("CastleBarGroup").SetActive(false);
+                }
+                break;
             case "Plains":
-                if (bosses.plainsBossDefeated)
-                {
+                if (bosses.plainsBossDefeated) {
                     hasBeenDefeated = true;
                 }
                 break;
             case "Desert":
-                if (bosses.desertBossDefeated)
-                {
+                if (bosses.desertBossDefeated) {
                     hasBeenDefeated = true;
                 }
                 break;
             case "Underwater":
-                if (bosses.underwaterBossDefeated)
-                {
+                if (bosses.underwaterBossDefeated) {
                     hasBeenDefeated = true;
                 }
                 break;
             case "Jungle":
-                if (bosses.jungleBossDefeated)
-                {
+                if (bosses.jungleBossDefeated) {
                     hasBeenDefeated = true;
                 }
                 break;
             case "Skyland":
-                if (bosses.skylandBossDefeated)
-                {
+                if (bosses.skylandBossDefeated) {
                     hasBeenDefeated = true;
                 }
                 break;
         }
 
-        if (hasBeenDefeated)
-        {
+        if (hasBeenDefeated) {
             Destroy(gameObject);
         }
     }
@@ -236,6 +254,55 @@ public class CutsceneManager : MonoBehaviour {
                 case "Castle":
                     PlayFinalBossEnd();
                     break;
+            }
+        }
+
+        if(collision.tag == "Player" && SceneManager.GetActiveScene().name == "Plains") {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            var progress = GameManager.instance.gameFile.gameProgression;
+
+            //If the desert has been cleared AND the desert gem cutscene has not been viewed
+            if(progress.defeatedBosses.desertBossDefeated && !progress.viewedCutscenes.desertGem) {
+                //Mark the desert gem as played
+                GameManager.instance.gameFile.gameProgression.viewedCutscenes.desertGem = true;
+
+                //If the underwater and jungle bosses have already been defeated
+                if(progress.viewedCutscenes.underwaterGem && progress.viewedCutscenes.jungleGem) {
+                    //Play final desert gem
+                    PlayFinalDesertGem();
+                } else {
+                    //Otherwise, play normal desert gem
+                    PlayActivateDesertGem();
+                }
+                //Otherwise, if underwater was cleared AND the underwater cutscene has not been viewed
+            } else if (progress.defeatedBosses.underwaterBossDefeated && !progress.viewedCutscenes.underwaterGem) {
+                //Mark the underwater cutscene as viewed
+                GameManager.instance.gameFile.gameProgression.viewedCutscenes.underwaterGem = true;
+
+                //If desert and jungle have been cleared, play final gem version. Otherwise, play normal.
+                if(progress.viewedCutscenes.desertGem && progress.viewedCutscenes.jungleGem) {
+                    PlayFinalUnderwaterGem();
+                } else {
+                    PlayActivateUnderwaterGem();
+                }
+                //Otherwise, just do all that again but with jungle
+            } else if (progress.defeatedBosses.jungleBossDefeated && !progress.viewedCutscenes.jungleGem) {
+                //Mark the jungle cutscene as viewed
+                GameManager.instance.gameFile.gameProgression.viewedCutscenes.jungleGem = true;
+
+                //If desert and underwater have been cleared, play final gem version. Otherwise, play normal.
+                if (progress.viewedCutscenes.desertGem && progress.viewedCutscenes.underwaterGem)
+                {
+                    PlayFinalJungleGem();
+                }
+                else
+                {
+                    PlayActivateJungleGem();
+                }
+                //IF IT'S NONE OF THOSE, check if skyland was cleared. If it was, castle cutscene.
+            } else if(progress.defeatedBosses.skylandBossDefeated && !progress.viewedCutscenes.castleGate) {
+                GameManager.instance.gameFile.gameProgression.viewedCutscenes.castleGate = true;
+                PlayOpenGate();
             }
         }
     }
